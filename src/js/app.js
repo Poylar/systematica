a75a9f613b159c24adfd143719e7d248aba83cfe
poylar <doaleks8@gmail.com>
2021-11-30 16:30:35 +0800

    lel

---
+++ src/js/app.js
@@ -0,0 +1,75 @@
+import '../scss/app.scss'
+
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
+  }
+
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+  var that = this;
+  var delta = 200 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 500;
+  }
+
+  setTimeout(function () {
+    that.tick();
+  }, delta);
+};
+
+window.onload = function () {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
+    }
+  }
+
+};
+
+
+
+const filterBtn = document.querySelector('[data-event="filter"]')
+const filterWrap = document.querySelector('.selection__filter');
+let isOpen = false;
+filterBtn.addEventListener('click', () => {
+  if (!isOpen) {
+    filterBtn.classList.add('active')
+    filterWrap.style.height = `${filterWrap.scrollHeight}px`
+    filterWrap.classList.add('active')
+    isOpen = true;
+  } else {
+    filterBtn.classList.remove('active')
+    filterWrap.style.height = `0px`
+    filterWrap.classList.remove('active')
+    isOpen = false;
+  }
+})
+
+
---
+++ webpack.config.js
@@ -0,0 +1,118 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const StylelintPlugin = require('stylelint-webpack-plugin');
+
+const isDev = process.env.NODE_ENV === 'development'
+const isProd = !isDev
+console.log(isDev)
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  mode: 'development',
+  entry: {
+    main: ['@babel/polyfill', '/js/app.js'],
+
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/[name].js'
+  },
+
+  devServer: {
+    hot: isDev,
+    port: 9000,
+    static: './dist',
+    watchFiles: 'src/**/*'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new StylelintPlugin({
+      context: path.resolve(__dirname, 'src'),
+      extensions: ['css', 'scss']
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "images", to: "images" },
+        { from: "fonts", to: "fonts" },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: '/index.html',
+      minify: {
+        collapseWhitespace: isProd
+      }
+    }),
+
+
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [
+                  require('autoprefixer'),
+                ],
+              },
+            },
+          },
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: './images/[name][ext]',
+        },
+      },
+      {
+        test: /\.(ttf|woff|woff2)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: './fonts/[name][ext]',
+        },
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+
+    ]
+  }
+}
